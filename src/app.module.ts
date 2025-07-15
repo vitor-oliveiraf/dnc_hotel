c2cbf5befe9e5b283d4aa2daca727df9948e8cb1
Vitor Oliveira <vitoro.fabiano@gmail.com>
2025-07-15 16:46:18 -0300

    feat: add @nestjs/throttler dependency and integrate ThrottlerModule in app.module.ts for rate limiting functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,27 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/users/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    //   ThrottlerModule.forRoot([
+    //     {
+    //       ttl: 5000,
+    //       limit: 5,
+    //     },
+    //   ]),
+    // ],
+    // providers: [
+    //   {
+    //     provide: APP_GUARD,
+    //     useClass: ThrottlerGuard,
+    //   },
+  ],
 })
 export class AppModule {}
